6e1748405a5f3ef966dc7d24ab5c3bdd6ecd78eb
Pascal Birchler <hello@pascalbirchler.ch>
2015-04-08 09:15:56 +0200

    Add wp_deploy and checktextdomain tasks

---
+++ grunt/wp_deploy.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dist: {
+    options: {
+      plugin_slug: 'admin-menu-manager',
+      svn_user   : 'wearerequired',
+      build_dir  : 'build',
+      assets_dir : 'assets'
+    }
+  }
+};
