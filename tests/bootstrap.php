7cbab1829026cfb93ca82b9c61baef7a86ed98ab
Silvan Hagen <neverything@gmail.com>
2015-04-26 16:14:43 +0200

    Almost yolo commit, played with tests, made them fail :D

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,6 +12,13 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
 
+activate_plugin( 'admin-menu-manager/admin-menu-manager.php' );
+echo "Installing Admin Menu Manager...\n";
+
+global $current_user;
+$current_user = new WP_User(1);
+$current_user->set_role('administrator');
+
 class Admin_Menu_Manager_TestCase extends WP_UnitTestCase {
 	function plugin() {
 		return Admin_Menu_Manager_Plugin::get_instance();
@@ -23,5 +30,21 @@ class Admin_Menu_Manager_TestCase extends WP_UnitTestCase {
 
 	function unset_post( $key ) {
 		unset( $_POST[$key], $_REQUEST[$key] );
+	}
+
+	/**
+	 * Utility to echo out actions.
+	 *
+	 * Taken from wordpress-develop/phpunit/includes/utils.php
+	 *
+	 * @param           $callable
+	 * @param   array   $args
+	 *
+	 * @return  string
+	 */
+	function get_echo( $callable, $args = array() ) {
+		ob_start();
+		call_user_func_array($callable, $args);
+		return ob_get_clean();
 	}
 }
--- tests/test-tests.php
+++ tests/test-tests.php
@@ -1,7 +1,25 @@
 <?php
 
+/**
+ * Class Admin_Menu_Manager_Test_Utils
+ *
+ * Utility class for the Admin Menu Manager Testing Suite.
+ */
 class Admin_Menu_Manager_Test_Utils extends Admin_Menu_Manager_TestCase {
+
 	function test_tests() {
 		$this->assertTrue( true );
+	}
+	function test_sample_string() {
+		$string = "Unit tests are sweet!";
+
+		$this->assertEquals( "Unit tests are sweet!", $string );
+		$this->assertNotEquals( "Unit tests suck!", $string );
+	}
+	function test_get_admin_menu() {
+
+		$menu_items = $this->plugin()->get_admin_menu();
+		$this->assertInternalType( 'array' , $menu_items );
+
 	}
 }
