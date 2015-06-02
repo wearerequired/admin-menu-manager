7eaf193a73a126b0d8c405ddfbb9f34fd114f22d
Pascal Birchler <hello@pascalbirchler.ch>
2015-06-02 13:46:05 +0200

    Delete old-style options from version 1.x on uninstall / reset

--- uninstall.php
+++ uninstall.php
@@ -12,5 +12,12 @@
 // If uninstall, not called from WordPress, then exit
 defined( 'WP_UNINSTALL_PLUGIN' ) or die;
 
+// Delete old-style options from version 1.x
 delete_option( 'amm_menu' );
 delete_option( 'amm_submenu' );
+
+// Delete options from version 2.x
+delete_metadata( 'user', null, 'amm_menu', '', true );
+delete_metadata( 'user', null, 'amm_submenu', '', true );
+delete_metadata( 'user', null, 'amm_trash_menu', '', true );
+delete_metadata( 'user', null, 'amm_trash_submenu', '', true );
